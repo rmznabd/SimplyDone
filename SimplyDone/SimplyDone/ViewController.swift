12d8bb046ab27319b9c5e112260bd324b7f32348
Ramazan <ramazan@backbase.com>
2025-02-27 22:29:08 +0100

    initial commit

---
+++ SimplyDone/SimplyDone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimplyDone
+//
+//  Created by Ramazan Abdullayev on 27/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+}
+
